f33d66a2b6009ba482e4ebc742ea07b664939941
Parzival-0x9 <chris.feliciano05@gmail.com>
2025-09-22 18:06:27 +1000

    feat: update site + set Google Sheets webhook URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
 // Set this to your Apps Script Web App URL to log bookings to a worksheet.
 // Example: https://script.google.com/macros/s/AKfycbx.../exec
-export const SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxnLHKVnK96sVemWNwNbZWxdudGYr6jvAdbq7fPIuJB774qj283LTj06vaed1CQfCPz1w/exec"; // leave empty to disable remote logging
+export const SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbyAaWTX5ldXRq2K1VW6niYFGhaL7N9y1PMlLwKVJus7x3XtmJTdpDHNgoKzmxcVU8dH6Q/exec"; // leave empty to disable remote logging
