b4a7d1472c084bb4d2e872a8490783e49c216646
Parzival-0x9 <chris.feliciano05@gmail.com>
2025-09-23 10:25:42 +1000

    Wipe old project files; fresh start

--- config.js
+++
@@ -1,4 +0,0 @@
-
-// Set this to your Apps Script Web App URL to log bookings to a worksheet.
-// Example: https://script.google.com/macros/s/AKfycbx.../exec
-export const SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzmAfLm8vItvhU5j5JPlLWLLCVIWAxQUA9yrAsp4Ju0rC1Hi73lnec9Bqg2-I-_ClLgag/exec"; // leave empty to disable remote logging
